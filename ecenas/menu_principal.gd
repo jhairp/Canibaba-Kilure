6bf35d230397d3408986190eb450ddfd42b9979a
jhair pinto <jhairpinto8@gmail.com>
2025-10-31 06:32:40 -0400

    ultimo avance

--- ecenas/menu_principal.gd
+++ ecenas/menu_principal.gd
@@ -6,7 +6,7 @@ func _ready():
 
 func _on_boton_jugar_pressed():
 	print("✅ Botón jugar presionado")
-	get_tree().change_scene_to_file("res://ecenas/Ecenas/primer_ecena.tscn")
+	get_tree().change_scene_to_file("res://ecenas/Ecenas/historia.tscn")
 
 func _on_boton_salir_pressed():
 	get_tree().quit()
